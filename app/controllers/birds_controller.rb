a9bdaa2fce87d7dcfac627daf0a815a139f13cf7
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-24 17:02:26 -0800

    code along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,16 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds
   end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      render json: bird
+    else
+      render json: { message : 'Bird not found'}
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/birds' => 'birds#index'
+  get '/birds/:id' => 'birds#show'
 end
